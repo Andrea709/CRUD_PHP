30e4b2d3abae3fe65dc1fb5bd6d5b2cef5c28eb1
Nog <63927458+Andrea709@users.noreply.github.com>
2023-06-20 11:18:10 -0300

    Continuando aqui

---
+++ php-mysql_CRUD/VideoAulas/cadastrar.php
@@ -0,0 +1,15 @@
+<h1>Cadastrar usuário<h1>
+
+<form method="POST" action="cadastrar_action.php">
+    <label>
+        Nome: <input type="text" name="nome" />
+    </label>
+    <label>
+        Idade: <input type="text" name="idade" />
+    </label>
+    <label>
+        Profissão: <input type="text" name="prof" />
+    </label>
+
+    <input type="submit" value="Salvar"/>
+</form>
